b5c2134b384827953837b2dcd963773efa36e534
VitorLgn <74212808+VitorLgn@users.noreply.github.com>
2020-11-10 18:03:55 -0300

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
 
 #This function is TOTALLY based on the functions shown on the assignment instructions.
-
-## Write a short comment describing this function
+#This function basically cache the matrix in another object
 
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL #the inverse
@@ -11,15 +8,15 @@ makeCacheMatrix <- function(x = matrix()) {
                 x <<- y #defining the 'x' matrix
                 i <<- NULL
         }
-        get <- function() x #taking the 'x' matrix
-        setinv <- function(inv) i <<- inv #defining the inverse of 'x'
-        getinv <- function() i  #geting the inverse of 'x'
+        get <- function() x 
+        setinv <- function(inv) i <<- inv 
+        getinv <- function() i  
         list(set = set, get = get, setinv = setinv,
              getinv = getinv) #way to return the previous objects
 }
-## Write a short comment describing this function
-#This function is TOTALLY based on the functions shown on the assignment instructions.
-#sorry for my bad english :) 
+
+#This function take the previously matrix cached on that object of the first function
+#and transform that matrix on the inverse with the function solve()
 cacheSolve <- function(x, ...) {
         i <- x$getinv() # applying on the 'x' the 'getinv' func.
         if(!is.null(i)) { #defining a condition, because if det(x) =/= 0, the inverse doesn't exist.
@@ -29,5 +26,5 @@ cacheSolve <- function(x, ...) {
         data <- x$get() 
         i <- solve(data, ...) #solve is the function in r that really show the inverse matrix
         x$setinv(i)
-        i   ## Return a matrix that is the inverse of 'x'
+        return(i)   ## Return a matrix that is the inverse of 'x'
 }
